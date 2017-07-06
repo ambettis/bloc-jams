12b19bfe1e3ffe5bcaf74fef17b0e8a58ce43860
Austin Bettis <austinbettis@Austins-MacBook-Pro.local>
2017-07-06 11:29:06 -0400

    checkpoint 12 play-pt1: completed

--- scripts/album.js
+++ scripts/album.js
@@ -46,7 +46,7 @@ var bamaChampionships = {
 var createSongRow = function (songNumber, songName, songLength){
     var template =
         '<tr class = "album-view-song-item">'
-     +      '<td class = "song-item-number">' + songNumber + '</td>'
+     +      '<td class = "song-item-number" data-song-number="' + songNumber +'">' + songNumber + '</td>'
      +      '<td class = "song-item-title">' + songName + '</td>'
      +      '<td class = "song-item-duration">' + songLength + '</td>'
      +  '</tr>'
@@ -72,16 +72,24 @@ var setCurrentAlbum = function(album){
     }
 };
 
-var albumsArray = [albumPicasso, albumMarconi, bamaChampionships];
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>'
 
 window.onload = function (){
-    setCurrentAlbum(albumsArray[0]);
+    setCurrentAlbum(albumPicasso);
+
+    songListContainer.addEventListener('mouseover', function(event){
+        if (event.target.parentElement.className === 'album-view-song-item'){
+            event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+        }
+    })
+
+    for (var i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+         });
+     }
 };
-
-
-document.getElementById("album-button").addEventListener("click", function(){
-    var first = albumsArray.shift();
-    albumsArray.push(first);
-    setCurrentAlbum(albumsArray[0]);
-    document.reload();
-});
