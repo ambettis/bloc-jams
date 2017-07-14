b08b89851de2d5200384537ce5089b691a6d60f4
Alex Spencer <alex@alexspencer.net>
2017-07-14 08:55:35 -0400

    fixes

--- scripts/album.js
+++ scripts/album.js
@@ -71,21 +71,25 @@ var setCurrentAlbum = function(album){
         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
     }
 };
-var findParentByClassName = funtion(element, targetClass){
-    if (element.parentElement = null){
+
+var findParentByClassName = function(element, targetClass){
+    if (element.parentElement == null){
         console.log('No parent found');
     }
+
     if (element) {
-        var currentParent = element.parentElement;
-        while (currentParent.className !== targetClass && currentParent.className !== null){
-            currentParent = currentParent.parentElement;
-        }
-        if (currentParent = null){
-            console.log('No parent found with that class name');
-            return;
-        }
-        return currentParent;
+      var currentParent = element.parentElement;
+      while (currentParent.className !== targetClass && currentParent.className !== null){
+          currentParent = currentParent.parentElement;
+      }
     }
+
+    if (currentParent == null){
+      console.log('No parent found with that class name');
+      return;
+    }
+
+    return currentParent;
 };
 
 var getSongItem = function(element){
