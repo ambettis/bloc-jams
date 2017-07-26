df321e9f0e6eb651e9a436760be6853ea5771a37
Austin Bettis <austinbettis@Austins-MacBook-Pro.local>
2017-07-26 09:27:06 -0500

    checlpoint 19 help needed

--- scripts/album.js
+++ scripts/album.js
@@ -1,48 +1,3 @@
-var albumPicasso = {
-    title: 'The Colors',
-    artist: 'Pablo Picasso',
-    label: 'Cubism',
-    year: '1881',
-    albumArtURL: 'assets/images/album_covers/01.png',
-    songs: [
-        {title: 'Blue', duration: '4:26'},
-        {title: 'Green', duration: '3:14'},
-        {title: 'Red', duration: '5:01'},
-        {title: 'Pink', duration: '3:21'},
-        {title: 'Magenta', duration: '2:15'},
-    ]
-};
-
-var albumMarconi = {
-    title: 'The Telephone',
-    artist: 'Guglielmo Marconi',
-    label: 'EM',
-    year: '1909',
-    albumArtURL: 'assets/images/album_covers/20.png',
-    songs: [
-       {title: 'Hello, Operator?', duration: '1:01' },
-       {title: 'Ring, ring, ring', duration: '5:01' },
-       {title: 'Fits in your pocket', duration: '3:21'},
-       {title: 'Can you hear me now?', duration: '3:14' },
-       {title: 'Wrong phone number', duration: '2:15'}
-    ]
-};
-
-var bamaChampionships = {
-    title: 'The Dynasty',
-    artist: 'Nick Saban',
-    label: 'SEC',
-    year: '2017',
-    albumArtURL: 'http://larrybrownsports.com/wp-content/uploads/2016/01/nick-saban-grin.jpg',
-    songs: [
-       {title: '2009 Bama v Texas', duration: '37:21' },
-       {title: '2011 Bama v LSU', duration: '21:00' },
-       {title: '2012 Bama v Notre Dame', duration: '42:14'},
-       {title: '2015 Bama v Clemson', duration: '45:40' },
-       {title: '20?? Bama v ??', duration: '??:??'}
-    ]
-};
-
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
         '<tr class="album-view-song-item">'
@@ -53,31 +8,34 @@ var createSongRow = function(songNumber, songName, songLength) {
     ;
 
     var $row = $(template);
-    var clickHandler = function() {
+var clickHandler = function() {
         var songNumber = $(this).attr('data-song-number');
-        if (currentlyPlayingSong !== null) {
-        var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
-        currentlyPlayingCell.html(currentlyPlayingSong);
+        if (currentlyPlayingSongNumber !== null) {
+        var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+        currentlyPlayingCell.html(currentlyPlayingSongNumber);
     }
-    if (currentlyPlayingSong !== songNumber) {
+    if (currentlyPlayingSongNumber !== songNumber) {
         $(this).html(pauseButtonTemplate);
-        currentlyPlayingSong = songNumber;
-    } else if (currentlyPlayingSong === songNumber) {
+        currentlyPlayingSongNumber = songNumber;
+        updatePlayerBarSong();
+    } else if (currentlyPlayingSongNumber === songNumber) {
         $(this).html(playButtonTemplate);
-        currentlyPlayingSong = null;
+        $(".main-controls .play-pause").html(playerBarPauseButton);
+        currentlyPlayingSongNumber = null;
+        currentSongFromAlbum = null;
     }
     };
     var onHover = function(event) {
         var songNumberCell = $(this).find('.song-item-number');
         var songNumber = songNumberCell.attr('data-song-number');
-        if (songNumber !== currentlyPlayingSong) {
+        if (songNumber !== currentlyPlayingSongNumber) {
             songNumberCell.html(playButtonTemplate);
         }
     };
     var offHover = function(event) {
         var songNumberCell = $(this).find('.song-item-number');
         var songNumber = songNumberCell.attr('data-song-number');
-        if (songNumber !== currentlyPlayingSong) {
+        if (songNumber !== currentlyPlayingSongNumber) {
             songNumberCell.html(songNumber);
         }
     };
@@ -88,6 +46,7 @@ var createSongRow = function(songNumber, songName, songLength) {
 
 
 var setCurrentAlbum = function(album){
+    var currentAlbum = album;
     var $albumTitle = $('.album-view-title');
     var $albumArtist = $('.album-view-artist');
     var $albumReleaseInfo = $('.album-view-release-info');
@@ -106,11 +65,93 @@ var setCurrentAlbum = function(album){
     }
 };
 
+// var updatePlayerBarSong = function() {
+//
+//     $('.currently-playing .song-name').text(currentSongFromAlbum.title);
+//     $('.currently-playing .artist-name').text(currentAlbum.artist);
+//     $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
+// };
+
+var updatePlayerBarSong = function(){
+  $(".currently-playing .artist-name").text(currentAlbum.artist);
+  $(".currently-playing .song-name").text(currentSongFromAlbum.title);
+  $(".currently-playing .artist-song-mobile").text(currentSongFromAlbum.title + " + " + currentAlbum.artist);
+  $(".main-controls .play-pause").html(playerBarPauseButton);
+};
+
+
+
+// var trackIndex = function(album,song){
+//   return album.songs.indexOf(song);
+// };
+
+// var nextSong = function() {
+//     var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+//     // Note that we're _incrementing_ the song here
+//     currentSongIndex++;
+//
+//     if (currentSongIndex >= currentAlbum.songs.length) {
+//         currentSongIndex = 0;
+//     }
+//
+//     // Save the last song number before changing it
+//     var lastSongNumber = currentlyPlayingSongNumber;
+//
+//     // Set a new current song
+//     currentlyPlayingSongNumber = currentSongIndex + 1;
+//     currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
+//
+//     // Update the Player Bar information
+//     updatePlayerBarSong();
+//
+//     var $nextSongNumberCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+//     var $lastSongNumberCell = $('.song-item-number[data-song-number="' + lastSongNumber + '"]');
+//
+//     $nextSongNumberCell.html(pauseButtonTemplate);
+//     $lastSongNumberCell.html(lastSongNumber);
+// };
+
+// var previousSong = function() {
+//     var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+//     // decrement the song here
+//     currentSongIndex--;
+//
+//     if (currentSongIndex < 0) {
+//         currentSongIndex = currentAlbum.songs.length - 1;
+//     }
+//
+//     // Save the last song number before changing it
+//     var lastSongNumber = currentlyPlayingSongNumber;
+//
+//     // Set a new current song
+//     currentlyPlayingSongNumber = currentSongIndex + 1;
+//     currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
+//
+//     // Update the Player Bar information
+//     updatePlayerBarSong();
+//
+//     var $nextSongNumberCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+//     var $lastSongNumberCell = $('.song-item-number[data-song-number="' + lastSongNumber + '"]');
+//
+//     $nextSongNumberCell.html(pauseButtonTemplate);
+//     $lastSongNumberCell.html(lastSongNumber);
+// };
+
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
-var currentlyPlayingSong = null;
+var playerBarPlayButton = '<span class = "ion-play"></span>';
+var playerBarPauseButton = '<span class = "ion-pause"></span>';
+
+var currentAlbum = null;
+var currentlyPlayingSongNumber = null;
+var currentSongFromAlbum = null;
+
+// var $previousButton = $('.main-controls .previous');
+// var $nextButton = $('.main-controls .next');
+
 
 $(document).ready(function(){
     setCurrentAlbum(albumPicasso);
-
+    // $previousButton.click(previousSong);
+    // $nextButton.click(nextSong);
 });
---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtURL: 'assets/images/album_covers/01.png',
+    songs: [
+        {title: 'Blue', duration: '4:26'},
+        {title: 'Green', duration: '3:14'},
+        {title: 'Red', duration: '5:01'},
+        {title: 'Pink', duration: '3:21'},
+        {title: 'Magenta', duration: '2:15'},
+    ]
+};
+
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtURL: 'assets/images/album_covers/20.png',
+    songs: [
+       {title: 'Hello, Operator?', duration: '1:01' },
+       {title: 'Ring, ring, ring', duration: '5:01' },
+       {title: 'Fits in your pocket', duration: '3:21'},
+       {title: 'Can you hear me now?', duration: '3:14' },
+       {title: 'Wrong phone number', duration: '2:15'}
+    ]
+};
+
+var bamaChampionships = {
+    title: 'The Dynasty',
+    artist: 'Nick Saban',
+    label: 'SEC',
+    year: '2017',
+    albumArtURL: 'http://larrybrownsports.com/wp-content/uploads/2016/01/nick-saban-grin.jpg',
+    songs: [
+       {title: '2009 Bama v Texas', duration: '37:21' },
+       {title: '2011 Bama v LSU', duration: '21:00' },
+       {title: '2012 Bama v Notre Dame', duration: '42:14'},
+       {title: '2015 Bama v Clemson', duration: '45:40' },
+       {title: '20?? Bama v ??', duration: '??:??'}
+    ]
+};
